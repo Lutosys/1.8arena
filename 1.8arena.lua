a8670e853ec2d015cc2de48dcb8f93394fd73f16
Lutosys <nickgaming271@gmail.com>
2026-05-25 20:59:24 -0400

    Remove SCRIPT_KEY assignment and load script

--- 1.8arena.lua
+++ 1.8arena.lua
@@ -1,2 +1,1 @@
-getgenv().SCRIPT_KEY = "keyless"
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/3eb3c0e92e617dec5991a5639ba101dcaa1285bc42b1fa215ab02fe047214021/download"))()
